04c8e6e33ab43fec0c918f9390a7eb2703de6dfe
sim0210 <sghgh0210@naver.com>
2023-03-05 21:59:17 +0900

    집에서 캐릭터 나오는것까지 작업
    집에서 캐릭터 나오는것까지 작업

--- InteractiveWEB/01/gsapIntro/example-01/js/main.js
+++ InteractiveWEB/01/gsapIntro/example-01/js/main.js
@@ -16,9 +16,9 @@ function init(){
 
   // 총괄 애니메이션
   const master = gsap.timeline();
-  master.add( FUNCTION_openingTL() );
-  master.add( FUNCTION_poringMoving() ) ;
-  // master.add( FUNCTION_mainScrollTrigger() ) ;
+  master.add( FUNCTION_openingTL );
+  // master.add( FUNCTION_mainScrollTrigger ) ;
+  // master.add( FUNCTION_poringMoving() ) ;
   // master.add( FUNCTION_openingTL_nextStep() );
   // master.add( FUNCTION_openingTL({duration : 1}).reverse() );
   
@@ -26,28 +26,25 @@ function init(){
 
   function FUNCTION_openingTL(){
     let openingTL = gsap.timeline();
-    openingTL.to("section.main", { opacity: 1, duration: 1 })  
-             .to(".mainEarth", { scale : 1, /*rotation : 360,*/ duration: 2, /* ease: "slow(0.7, 0.7, false)" */ })
-              //  .to(".box", { duration: 0.2, stagger: {each: 0.2, from: "center", ease: "power2.inOut" } },"<")
+    openingTL.to("section.main", { opacity: 1, duration: 0.5, ease: "power4.in" })  
+             .to(".mainEarth", { scale : 1, opacity: 1, /*rotation : 360,*/ duration: 0.5, /* ease: "slow(0.7, 0.7, false)" */ })
+              
+              // .to(".box", {  opacity: 1, scale : 1, ease:"bounce.in", duration : 0.5, stagger: { grid: [7,15], from: "center", amount: 1 } } ,"<-=0.2") // 튀어나옴
+              .from(".box", { opacity: 0, left:"50%", top:"50%", scale : 0, ease:"bounce.in", duration : 0.8, stagger: { from : "center",  amount: 0.8 } })  // 흩뿌림
+              
+              .to(".earthWrap--text1", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" },"<+=0.3")
 
-              .to(".earthWrap--backgroundTitle", {  opacity: 1, duration : 2.5,},"<+=2")
-              // .to(".earthWrap--text1", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" },"<-=2.3") // 좀 빠른 버전
-              .to(".earthWrap--text1", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" },"<-=0.3")
-              .set(".earthWrap--playOn", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" },"<+=0.6")
+              .to(".earthWrap--with", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" },"<+=0.2")
+              .to(".earthWrap--gravity", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" },"<+=0.1")
+              .to(".earthWrap--backgroundTitle", {  opacity: 1, duration : 2.5,},"<")
+              
+              .set(".earthWrap--playOn", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" },"<+=1.0")
               .add(FUNCTION_playonTL,"<")
-              .to(".earthWrap--with", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" },"<+=3")
-              .to(".earthWrap--gravity", {  opacity: 1, y:50, duration : 0.8, ease: "power1.inOut" })
+              .add(FUNCTION_mainScrollTrigger,"<+=1" )
 
-
-             .to(".box.main--1", {  opacity: 1, motionPath : PATH_LIST.main_1_path , duration : 0.9, },"<+=0.5")
-             .to(".box.main--o", {  opacity: 1, motionPath : PATH_LIST.main_o_path , duration : 0.9,},"<+=0.3")
-             .to(".box.main--a", {  opacity: 1, motionPath : PATH_LIST.main_a_path , duration : 0.9,},"<+=0.2")
-             .to(".box.main--b", {  opacity: 1, motionPath : PATH_LIST.main_b_path , duration : 0.9,},"<+=0.1")
-             .to(".box.main--l", {  opacity: 1, motionPath : PATH_LIST.main_l_path , duration : 0.9,},"<+=0.2")
-             .to(".box.main--e", {  opacity: 1, motionPath : PATH_LIST.main_e_path , duration : 0.9,},"<+=0.1")
-             
-
-             .to(".box.main--j", {  opacity: 1, motionPath : PATH_LIST.main_o_path , duration : 0.9,},"<+=0.1")
+              
+              
+                         
              
             
     return openingTL;
@@ -76,21 +73,34 @@ function init(){
 
   function FUNCTION_playonTL(){
     let playonTL = gsap.timeline()
-    .to('.earthWrap--playOn .object1, .earthWrap--playOn .object2', { opacity : 1, })
-    .to('.earthWrap--playOn .object1', { x: -32, duration : 0.4 })
-    .to('.earthWrap--playOn .object2', { x: 126, duration : 0.4 }, "<")
-    .to('.earthWrap--playOn .poring', { opacity :1, scale :1, duration :0.8, ease : "bounce.in" })
+    .to('.earthWrap--playOn .object1', { display : 'inline-block', duration : 0.3 },"<+=0.3")
+    .to('.earthWrap--playOn .textP', { display : 'inline-block', duration : 0.3 },"<+=0.3")
+    .to('.earthWrap--playOn .textL', { display : 'inline-block', duration : 0.3 },"<+=0.3")
+    .to('.earthWrap--playOn .textA', { display : 'inline-block', duration : 0.3 },"<+=0.3")
+    .to('.earthWrap--playOn .textY', { display : 'inline-block', duration : 0.3 },"<+=0.3")
+
+
+    .to('.earthWrap--playOn .poring', { display : 'inline-block', duration : 0.3 },"<+=0.3")
+    .to('.earthWrap--playOn .textN', { display : 'inline-block', duration : 0.3 },"<+=0.3")
+    .to('.earthWrap--playOn .object2', { display : 'inline-block', duration : 0.3 },"<+=0.3")
+    .to('.earthWrap--playOn .cursor', { duration :0.01, opacity : 0, display :'none', scale : 0, color: "rgba(0,0,0,0)", })
+    .add(FUNCTION_earthMoving)
+
+    // .to('.earthWrap--playOn .object1, .earthWrap--playOn .object2', { opacity : 1, })
+    // .to('.earthWrap--playOn .object1', { x: -32, duration : 0.4 })
+    // .to('.earthWrap--playOn .object2', { x: 126, duration : 0.4 }, "<")
+    // .to('.earthWrap--playOn .poring', { opacity :1, scale :1, duration :0.8, ease : "bounce.in" })
     
     
-    .to('.earthWrap--playOn .object1', { x: -327, duration : 0.3 })
-    .to('.earthWrap--playOn .object2', { x: 408 , duration : 0.3 } ,"<")
-    .to('.earthWrap--playOn .poring', { x: 161 } ,"<")
-    .to('.earthWrap--playOn .textN', { opacity :1, x: 300, duration : 0.2 } ,"<")
+    // .to('.earthWrap--playOn .object1', { x: -327, duration : 0.3 })
+    // .to('.earthWrap--playOn .object2', { x: 408 , duration : 0.3 } ,"<")
+    // .to('.earthWrap--playOn .poring', { x: 161 } ,"<")
+    // .to('.earthWrap--playOn .textN', { opacity :1, x: 300, duration : 0.2 } ,"<")
     
-    .to('.earthWrap--playOn .textP', { opacity :1, x: -289 , duration : 0.2 } ,"<+=0.4")
-    .to('.earthWrap--playOn .textL', { opacity :1, x: -182 , duration : 0.2 } ,"<+=0.3")
-    .to('.earthWrap--playOn .textA', { opacity :1, x: -79  , duration : 0.2} ,"<+=0.2")
-    .to('.earthWrap--playOn .textY', { opacity :1, x: -70  , duration : 0.2} ,"<+=0.1")
+    // .to('.earthWrap--playOn .textP', { opacity :1, x: -289 , duration : 0.2 } ,"<+=0.4")
+    // .to('.earthWrap--playOn .textL', { opacity :1, x: -182 , duration : 0.2 } ,"<+=0.3")
+    // .to('.earthWrap--playOn .textA', { opacity :1, x: -79  , duration : 0.2} ,"<+=0.2")
+    // .to('.earthWrap--playOn .textY', { opacity :1, x: -70  , duration : 0.2} ,"<+=0.1")
     
 
     return playonTL;
@@ -155,36 +165,89 @@ init();
   }
 
 
-
-  function FUNCTION_mainScrollTrigger() { 
-    let mainScrollTrigger = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".main",
-        pin: true,
-        start: "center bottom",
-        end: "center top",
-        scrub: 3,
-        markers : true
-        
-
-        
-      }
+  //earth moving
+  function FUNCTION_earthMoving(){
+    const earthMovingTl = gsap
+    .timeline({
+      repeat: -1,
+      // yoyo: true            
     })
-    // .to(".img1", { scale: 1.0 })
-    // .to("section.first .textBox2 h1.textYellow", {
-    //   duration: 0.6,
-    //   opacity: 1,
-    //   x: 250
-    // })
+    .set(".mainEarth", {
+      // rotation: 360,
+      transformOrigin: "center"
+    })
+    .to(".mainEarth", {
+      rotation: -360,
+      transformOrigin: "center",
+      ease:Linear.easeNone,
+      duration : 8
+    });
 
-    return mainScrollTrigger;
+    return earthMovingTl;
   }
 
 
 
+  function FUNCTION_mainScrollTrigger() { 
 
+    // gsap.to("body", { overflow : "visible" })
+
+    let mainScrollTrigger = gsap.timeline({
+            
+      scrollTrigger: {
+        trigger: '.main',
+        markers: true,
+        scrub: 1,
+        pin: true,
+                
+        end: () => "+=" + document.querySelector(".main").offsetHeight
+      }
+              
+      
+    })
+    .to(".earthWrap--playOn", { y : -180, scale : 0.8, duration : 0.2,})
+    .to(".mainEarth", { scale : 0.6 , duration : 0.2,}, "<")
+    
+
+    .to(".earthWrap--backgroundTitle", { opacity :0 , display :"none" , duration : 0.3,}, "<")
+    .to(".earthWrap--text1", {  opacity: 0,  duration : 0.1, },"<")
+    .to(".earthWrap--with", {  opacity: 0,  duration : 0.1, },"<")
+
+    .to(".earthWrap--gravity", {  opacity: 0,  duration : 0.1, },"<")
+    
+    // .to(".mainEarth", { scale : 0.9},"<")
+            
+    .to(".mainEarth", { scale : 1, duration : 0.3})
+    .to(".earthWrap--textGravityGames", {  opacity: 1, scale : 1, duration : 0.3, },"<")
+    .to(".earthWrap--textInfo1", {  opacity: 1,  duration : 0.3, },)
+    .to(".earthWrap--textInfo1", {  opacity: 0,  duration : 0.3, },)
+    .to(".earthWrap--textInfo2", {  opacity: 1,  duration : 0.3, }, "<+=0.5")
+    .set(".earthWrap--mainCharImgWrap", { opacity : 1 }, "<" )
+    .from(".mainSectionImg", { x : "-50%", opacity : 0, ease:"power4.inOut", duration : 0.8, stagger: { amount: 0.8 } }, "<") 
+
+    .to(".earthWrap--imgMainLine1", {  opacity: 1, height : 429,  duration : 0.3, },"<+=0.5")
+    .to(".earthWrap--imgMainLine2", {  opacity: 1, scale : 1, duration : 0.3, },"<+=0.2")
+    .to(".earthWrap--imgcenterLight", {  opacity: 1, scale : 1, duration : 0.3, },"<")
+    .to(".earthWrap--textInfo1", {  opacity: 0,  duration : 0.3, }, "<+=0.5")
+
+
+
+
+
+
+
+
+
+    
+    
+    // .to(".box", { left:"50%", top:"50%", width : 100, height : 100, ease:"bounce.inOut", duration : 0.8, stagger: { amount: 0.8 } }, "<") 
+  
+
+    return mainScrollTrigger;
+  }
 
   
+    
   // 랜덤으로 여러군대 뿌려주기
   // gsap.to(".notes", {
   //   y: gsap.utils.random(-50, -100, 10, true),
@@ -197,3 +260,8 @@ init();
   //     repeat: -1
   //   }
   // });
+
+
+
+  // 커서 관련 
+  let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: -1});
