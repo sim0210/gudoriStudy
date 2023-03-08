67f55d3326aed17e4fe41c07cdec03bbaaffbae2
sim0210 <sghgh0210@naver.com>
2023-03-08 17:52:56 +0900

    용범님꺼 추가 커밋

--- InteractiveWEB/01/gsapIntro/example-01/js/main.js
+++ InteractiveWEB/01/gsapIntro/example-01/js/main.js
@@ -1,6 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+  setTimeout(()=> {
+    window.scrollTo(0,0);
+  },50)
+  disableScroll();
+});
+
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, MotionPathPlugin);
 
-
+// 스크롤 스무터 구동.
 let smoother = ScrollSmoother.create({
     smooth: 2,   
     effects: true
@@ -8,15 +15,15 @@ let smoother = ScrollSmoother.create({
 
 
 
-
-
-  // gsap.to(".mainEarth", { opacity: 0.5, duration: 0.5, repeat: -1, yoyo: true });
-
+  
 function init(){
-
+  
   // 총괄 애니메이션
   const master = gsap.timeline();
   master.add( FUNCTION_openingTL );
+  master.add(FUNCTION_contentNextOBJMoving)
+  master.add(FUNCTION_titleDisplayTL)
+  master.add(FUNCTION_splitTextTL)
   // master.add( FUNCTION_mainScrollTrigger ) ;
   // master.add( FUNCTION_poringMoving() ) ;
   // master.add( FUNCTION_openingTL_nextStep() );
@@ -28,8 +35,7 @@ function init(){
     let openingTL = gsap.timeline();
     openingTL.to("section.main", { opacity: 1, duration: 0.5, ease: "power4.in" })  
              .to(".mainEarth", { scale : 1, opacity: 1, /*rotation : 360,*/ duration: 0.5, /* ease: "slow(0.7, 0.7, false)" */ })
-              
-              // .to(".box", {  opacity: 1, scale : 1, ease:"bounce.in", duration : 0.5, stagger: { grid: [7,15], from: "center", amount: 1 } } ,"<-=0.2") // 튀어나옴
+                            
               .set(".box", { scale : 1},"<")
               .from(".box", { opacity: 0, left:"50%", top:"50%", ease:"power1.in", duration : 0.3, stagger: { from : "center",  amount: 0.8 } },"<")  // 흩뿌림
               
@@ -41,7 +47,8 @@ function init(){
               
               .set(".earthWrap--playOn", {  opacity: 1, y:50, duration : 0.8, ease: "power1.in" },"<+=1.0")
               .add(FUNCTION_playonTL,"<")
-              .add(FUNCTION_mainScrollTrigger,"<+=1" )
+              .add(FUNCTION_mainScrollTrigger,"<+=1.5" )
+              .add(enableScroll,"<" )
                                                                               
     return openingTL;
 
@@ -88,17 +95,83 @@ function init(){
   }
 
 
+
+  function FUNCTION_splitTextTL(){
+    // kill();
+    var $text = $(".theWorldContent--textInfo p"),
+    mySplitText = new SplitText($text, {type:"words"});
+    gsap.set($text, {perspective:400});
+    
+    const splitTextTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.theWorldContent--textInfo',        
+        scrub: 3,  
+        markers : true,
+        start : "top-=100 80%",
+        end : "bottom 80%",
+        duration : 0.8
+
+
+      }
+    });
+    mySplitText.split({type:"chars, words"}) 
+    
+    splitTextTimeline.from(mySplitText.chars, {duration: 1.5, scale:10, autoAlpha:0, rotationX:-180,  transformOrigin:"100% 50%", ease:"back", stagger: 0.02});
+    
+    function kill(){
+      splitTextTimeline.clear().time(0);
+      mySplitText.revert();
+    };
+
+    return splitTextTimeline;
+    
+  }
+
+
+
+
+  function FUNCTION_titleDisplayTL(){
+
+
+    let titleDisplayTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'section .theWorldContent--playOn',        
+        scrub: 3,  
+        markers : true,
+        start : "top-=100 80%",
+        end : "bottom 80%",
+        duration : 0.8
+
+
+      }
+    });
+    
+    titleDisplayTl.set(".theWorldContent--playOn", {                
+        "clip-path": "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)"
+    })
+    
+    .to(".theWorldContent--playOn", { 
+    "clip-path": "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)" },">")    
+
+    .to(".theWorldContent--playOn", { 
+    "clip-path": "polygon(0% 0%, 45% 0%, 35% 100%, 0% 100%)" },">")
+
+    .to(".theWorldContent--playOn", {
+    "clip-path": "polygon(0% 0%, 85% 0%, 85% 100%, 0% 100%)" },">")
+
+    .to(".theWorldContent--playOn", {
+        "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" },">")
+    .from(".titWorld", { y : 100, opacity : 0 })
+    
+
+    return titleDisplayTl;
+
+  }
+
+
 };
 
 init();
-
-
-
-
-
-
-
-
 
 
   // 함수부분 ---------
@@ -148,6 +221,16 @@ init();
 
 
 
+  // contentNext object moving
+  function FUNCTION_contentNextOBJMoving(){
+    const contentNextOBJMoving = gsap.timeline({ })
+    .to(".objectBox", { y: 20, rotation :-10, ease: "bounce.out", duration : 1.5, stagger: { from: "center", each :0.1, repeat : -1, yoyo:true } }) 
+
+    return contentNextOBJMoving;
+  }
+
+
+
   function FUNCTION_mainScrollTrigger() { 
 
     // gsap.to("body", { overflow : "visible" })
@@ -180,7 +263,7 @@ init();
     .to(".mainEarth", { scale : 1, duration : 0.3})
     .to(".earthWrap--textGravityGames", {  opacity: 1, scale : 1, duration : 0.3, },"<")
     .to(".earthWrap--textInfo1", {  opacity: 1,  duration : 0.3, },)
-    .to(".alphabet", { left:"50%", top:"60%", width : 100, height : 100, x: "-50%", y: "-50%", ease:"power4.inOut", duration : 0.8, stagger: { amount: 0.8 } }, "<") 
+    .to(".alphabet", { left:"50%", top:"60%", width : 100, height : 100, x: "-50%", y: "-50%", ease:"power4.inOut", duration : 0.5, stagger: { amount: 0.1 } }, "<") 
     .to(".earthWrap--textInfo1", {  opacity: 0,  duration : 0.3, },)
     
     
@@ -189,28 +272,30 @@ init();
     .to(".alphabet.rightOut", {  x: "1000%",  ease:"power4.inOut", opacity :0, duration : 1.2, stagger: { amount: 0.8 } }, "<") 
     .set(".earthWrap--mainCharImgWrap", { opacity : 1 }, "<+=1.0" )
     .from(".mainSectionImg", { x : "-50%", opacity : 0, ease:"power4.inOut", duration : 0.8, stagger: { amount: 0.8 } }, "<") 
-
+    
     .to(".earthWrap--imgMainLine1", {  opacity: 1, height : 429,  duration : 0.3, },"<+=0.5")
     .to(".earthWrap--imgMainLine2", {  opacity: 1, scale : 1, duration : 0.3, },"<+=0.2")
     .to(".earthWrap--imgcenterLight", {  opacity: 1, scale : 1, duration : 0.3, },"<")
     .to(".earthWrap--textInfo1", {  opacity: 0,  duration : 0.3, }, "<+=0.5")
-
+    
     .set("section.main", { backgroundColor:"#ffffff" })
     .set("section.main .content", { backgroundColor:"#000010",  },"<" )
     .to("section.main .content", { scale:0, overflow: "hidden", borderRadius : "1500px", duration : 1.5, ease:"none",}, "<") 
     
-        
+    
     .set(".contentNext", { display : "flex"},"<+=0.7") 
     .to(".contentNext", { opacity:1, scale: 1, duration : 1.0, ease:"none",}, "<") 
     .to(".onlineEarthWrap", { x:0, y:"-50%", scale :1,  duration : 2.0, ease:"none",}, "<")
-    .to(".onlineEarthWrap--power", { scale :1,  duration : 1.0, ease:"bounce.out",},"<+=0.6")
+    .to(".onlineEarthWrap--power", { scale :1,  duration : 1.0, ease:"bounce.in",},"<+=0.6")
+    .from(".objectBox", { scale :0, ease:"bounce.inOut", duration : 0.8, stagger: { amount: 0.8 } }, "<") 
+    
     
     .to(".onlineEarthWrap", { x:"-80%", y:"20%", scale :0, opacity : 0,  duration : 2.0, ease:"none",}) 
         
 
     .to(".text-online", { opacity :0, x : "-70%" , scale:0 },"<+=0.3")
     .to(".mobilePhoneWrap", { x:0, y:"-50%", scale :1, opacity : 1, duration : 2.0, ease:"none",}, "<")
-    .to(".mobilePhoneWrap--power", { scale :1,  duration : 1.0, ease:"bounce.out",}, "<+=0.5")
+    .to(".mobilePhoneWrap--power", { scale :1,  duration : 1.0, ease:"bounce.in",}, "<+=0.5")
     .to(".text-mobile", { opacity :1, x : "-50%" },"<")
 
 
@@ -219,9 +304,10 @@ init();
             
     .to(".mobilePhoneWrap", { x:"-80%", y:"20%", scale :0, opacity : 0,  duration : 2.0, ease:"none",})     
     .to(".iptvWrap", { x:100, y:"-50%", scale :1, opacity : 1, duration : 1.0, ease:"none",}, "<")
-    .to(".iptvWrap--power", { scale :1,  duration : 1.0, ease:"bounce.out",}, "<+=0.5")
+    .to(".iptvWrap--power", { scale :1,  duration : 1.0, ease:"bounce.in",}, "<+=0.5")
     .to(".text-mobile", { opacity :0, x : "-70%" , scale:0 },"<")
     .to(".text-iptv", { opacity :1, x : "-50%" },"<+=0.3")
+
 
     return mainScrollTrigger;
   }
@@ -243,5 +329,5 @@ init();
 
 
 
-  // 커서 관련 
+  // play on 랜더링 부분 커서 관련 
   let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: -1});
---
+++ InteractiveWEB/01/gsapIntro/example-dragon/js/main.js
@@ -0,0 +1,212 @@
+document.addEventListener("DOMContentLoaded", function(){
+  setTimeout(()=> {
+    window.scrollTo(0,0);
+  },50)
+  // disableScroll();
+});
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, MotionPathPlugin);
+
+// 스크롤 스무터 구동.
+let smoother = ScrollSmoother.create({
+    smooth: 2,   
+    effects: true
+  });
+
+
+
+  
+function init(){
+  
+  // 총괄 애니메이션
+  const master = gsap.timeline();
+  master.add( FUNCTION_openingTL );
+
+  
+  function FUNCTION_openingTL(){
+    let openingTL = gsap.timeline()
+    .to("section.main", { opacity: 1, duration: 0.5, ease: "power4.in" })  
+    .from(".content--videoWrap", { scale: 1.7, y:"122%", x:"56%", opacity :0, duration: 0.5, ease: "" })  
+    .to(".content--videoWrap", {  x :"-78%", duration: 0.5, ease: "" })
+    .to(".text-playon", { opacity : 1, duration: 0.8, ease: "" })
+    .to(".text-on", { opacity : 1, y:0, duration: 0.5, ease: "" },"<+=0.3")
+    .to(".text-experience", { opacity : 1, x:0, duration: 0.5, ease: "" },"<+=0.3")
+    .to(".text-with", { opacity : 1, x:0, duration: 0.5, ease: "" },"<")
+    .to(".text-hypen", { opacity : 1, x:0, duration: 0.5, ease: "" },"<")
+    // .to(".text-gravity", { opacity : 1, x:0, duration: 0.5, ease: "" },"<")
+    
+    .to(".text-gravity",  { duration : 0.8, "clip-path": "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)" },"<") 
+    .to(".text-gravity",  { duration : 0.8, "clip-path": "polygon(0% 0%, 45% 0%, 35% 100%, 0% 100%)" },"<") 
+    .to(".text-gravity",  { duration : 0.8, "clip-path": "polygon(0% 0%, 85% 0%, 85% 100%, 0% 100%)" },"<") 
+    .to(".text-gravity",  { duration : 0.8, "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" },"<")
+    
+    .to(".content--videoWrap", { x :"50%" , scale : 3.5, opacity :0.8, duration: 0.5, ease: "" },"<+=0.5")
+    .to( ".text-playon", { duration :0.7, attr: { src: "images/tit-play.png" } },"<")
+    .to("#mainVideo", { opacity :1 ,duration: 0.7, ease: "" },"<")
+    .to(".text-hypen", { scaleX : 0.4 ,duration: 1.5, ease: CustomEase.create("custom", "M0,0 C0.14,0 0.242,0.438 0.272,0.561 0.313,0.728 0.354,0.963 0.362,1 0.37,0.985 0.414,0.873 0.455,0.811 0.51,0.726 0.573,0.753 0.586,0.762 0.662,0.812 0.719,0.981 0.726,0.998 0.818,0.998 1,1 1,1 ") })
+    .to(".text-with", { x : 92 ,duration: 1.5, ease: CustomEase.create("custom", "M0,0 C0.14,0 0.242,0.438 0.272,0.561 0.313,0.728 0.354,0.963 0.362,1 0.37,0.985 0.414,0.873 0.455,0.811 0.51,0.726 0.573,0.753 0.586,0.762 0.662,0.812 0.719,0.981 0.726,0.998 0.818,0.998 1,1 1,1 ") },"<")
+                                                                              
+    return openingTL;
+
+  }                         
+
+
+  // img src 변경
+  // gsap.set(img, { attr: { src: newSRC } });
+
+  function FUNCTION_openingTL_nextStep() {
+    let nextStepTl = gsap.timeline({repeat: 1}) 
+    
+    .to(".box.main--o", { scale : 0 })
+    .to(".box.main--o", {  x: -105.55, scale : 0, duration : 1.5, ease: "power1.inOut" })
+    .set(".box.main--o", {  x: 906, scale : 0} )
+    .to(".box.main--o", { scale : 1 })
+    .to(".box.main--o", {  x: -105.55,  duration : 5.5, ease: "power1.inOut" })
+    .to(".box.main--o", { scale : 0, duration : 1 },"<+=4.7")
+    .to(".box.main--o", {  x: 6, scale : 1} )
+
+    return nextStepTl;
+  }
+
+  
+
+  function FUNCTION_playonTL(){
+    let playonTL = gsap.timeline()
+    .to('.earthWrap--playOn .object1', { display : 'inline-block', duration : 0.2 },"<+=0.2")
+    .to('.earthWrap--playOn .textP', { display : 'inline-block', duration : 0.2 },"<+=0.2")
+    .to('.earthWrap--playOn .textL', { display : 'inline-block', duration : 0.2 },"<+=0.2")
+    .to('.earthWrap--playOn .textA', { display : 'inline-block', duration : 0.2 },"<+=0.2")
+    .to('.earthWrap--playOn .textY', { display : 'inline-block', duration : 0.2 },"<+=0.2")
+
+
+    .to('.earthWrap--playOn .poring', { display : 'inline-block', duration : 0.2 },"<+=0.2")
+    .to('.earthWrap--playOn .textN', { display : 'inline-block', duration : 0.2 },"<+=0.2")
+    .to('.earthWrap--playOn .object2', { display : 'inline-block', duration : 0.2 },"<+=0.2")
+    .to('.earthWrap--playOn .cursor', { duration :0.01, opacity : 0, display :'none', scale : 0, color: "rgba(0,0,0,0)", })
+    .add(FUNCTION_earthMoving)
+
+    
+
+    return playonTL;
+  }
+
+
+
+  function FUNCTION_splitTextTL(){
+    // kill();
+    var $text = $(".theWorldContent--textInfo p"),
+    mySplitText = new SplitText($text, {type:"words"});
+    gsap.set($text, {perspective:400});
+    
+    const splitTextTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.theWorldContent--textInfo',        
+        scrub: 3,  
+        markers : true,
+        start : "top-=100 80%",
+        end : "bottom 80%",
+        duration : 0.8
+
+
+      }
+    });
+    mySplitText.split({type:"chars, words"}) 
+    
+    splitTextTimeline.from(mySplitText.chars, {duration: 1.5, scale:10, autoAlpha:0, rotationX:-180,  transformOrigin:"100% 50%", ease:"back", stagger: 0.02});
+    
+    function kill(){
+      splitTextTimeline.clear().time(0);
+      mySplitText.revert();
+    };
+
+    return splitTextTimeline;
+    
+  }
+
+
+
+
+
+
+};
+
+init();
+
+
+  // 함수부분 ---------
+
+  //poring moving
+  function FUNCTION_poringMoving(){
+    const poringMovingTl = gsap
+    .timeline({
+      repeat: -1,
+      yoyo: true            
+    })
+    .set(".poring", {
+      rotation: 5,
+      transformOrigin: "center"
+    })
+    .to(".poring", {
+      rotation: -5,
+      transformOrigin: "center",
+      ease: "power1.inOut",
+      duration : 1
+    });
+
+    return poringMovingTl;
+  }
+
+
+  // contentNext object moving
+  function FUNCTION_contentNextOBJMoving(){
+    const contentNextOBJMoving = gsap.timeline({ })
+    .to(".objectBox", { y: 20, rotation :-10, ease: "bounce.out", duration : 1.5, stagger: { from: "center", each :0.1, repeat : -1, yoyo:true } }) 
+
+    return contentNextOBJMoving;
+  }
+
+
+
+  function FUNCTION_mainScrollTrigger() { 
+
+    // gsap.to("body", { overflow : "visible" })
+
+    let mainScrollTrigger = gsap.timeline({
+            
+      scrollTrigger: {
+        trigger: '.main',
+        markers: true,
+        scrub: 3,
+        pin: true,
+                
+        end: () => "+=" + document.querySelector(".main").offsetHeight*3
+      }
+              
+      
+    })
+    .to(".earthWrap--playOn", { y : -180, scale : 0.8, duration : 0.2,})
+   
+
+
+    return mainScrollTrigger;
+  }
+
+  
+    
+  // 랜덤으로 여러군대 뿌려주기
+  // gsap.to(".notes", {
+  //   y: gsap.utils.random(-50, -100, 10, true),
+  //   x: gsap.utils.random(-50, 50, 25, true),
+  //   opacity: 1,
+  //   duration: gsap.utils.random(1.5, 3, 1.5, true),
+  //   stagger: {
+  //     each: 0.5,
+  //     ease: "sine.in",
+  //     repeat: -1
+  //   }
+  // });
+
+
+
+  // play on 랜더링 부분 커서 관련 
+  let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: -1});
